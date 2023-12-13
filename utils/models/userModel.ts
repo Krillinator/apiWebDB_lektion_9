12e1eacbb1002726671c3f9dc57fa99f8fadf54e
Kristoffer Johansson <56152114+Krillinator@users.noreply.github.com>
2023-12-13 12:19:54 +0100

    init commit

---
+++ utils/models/userModel.ts
@@ -0,0 +1,29 @@
+import { User } from "@/utils/types/user"
+import mongoose, { Schema } from "mongoose"
+
+const userSchema = new Schema<User>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    age: {
+      type: Number,
+      required: true,
+    },
+  },
+  { strict: true }
+)
+
+userSchema.methods.fullName = function () {
+  return `${this.name}` // Debug
+}
+
+export const UserModel =
+  mongoose.models.UserModel ||
+  mongoose.model<User>("UserModel", userSchema, "users")
